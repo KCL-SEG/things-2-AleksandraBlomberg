4749a59dc6db7acca2c8950fe7f419af5d46b9fa
root <aleksandra.blomberg@kcl.ac.uk>
2023-12-07 15:56:54 +0000

    fixed things 2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,16 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
+from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(attrs={'maxlength': '120'}),
+            'quantity': forms.NumberInput()
+        }
+    
+    name = forms.CharField(min_length=1, max_length=35) 
+    quantity = forms.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)]) 
